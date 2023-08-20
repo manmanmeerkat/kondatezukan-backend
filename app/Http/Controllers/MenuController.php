af34b0d23df1cebb1d5288897381c827615be662
manmanmeer <masak6161@gmail.com>
2023-08-20 11:28:13 +0900

    update以外

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $menu = Menu::all();
+        return response()->json($menu);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $menu = Menu::create($request->all());
+        return $menu
+            ? response()->json($menu, 201)
+            : response()->json([], 500);
+        
+
+        $menu = new Menu;
+        $menu->name = $request->input('name');
+        // $menu->memo = $request->input('memo');
+        $menu->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $menu = Menu::find($id);
+        $menu->delete();
+    }
+}
